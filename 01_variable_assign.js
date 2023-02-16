85e7623daa13e3b96eaeabfd61984cd3974d78af
tushar0311 <tushar0311@gmail.com>
2023-02-16 14:12:01 +0530

    JS Variable declaration and initialization
    JS Variable declaration and initialization

---
+++ 01_variable_assign.js
@@ -0,0 +1,29 @@
+console.log("*****************************Step1**************************");
+
+var myFullName;
+console.log(myFullName);
+var myFullName="My Name is: Tushar Borade";
+console.log(myFullName);
+myFullName="My Full Name is: Tushar Gorakhnath Borade";
+console.log(myFullName);
+
+console.log("*****************************Step2**************************");
+
+console.log("I Love only JS");
+
+console.log("*****************************Step3**************************");
+
+var firstName="My First Name is : Tushar";
+console.log(firstName);
+var lastName="My Last Name is : Borade";
+console.log(lastName);
+var age="Age : 31";
+console.log(age);
+var address="Address : A/P Shrigonda.";
+console.log(address);
+var pinCode="Pincode : 413701";
+console.log(pinCode);
+var gender="Gender : Male";
+console.log(gender);
+var isMarried="Married ? : Yes";
+console.log(isMarried);
--- helloWord.js
+++ helloWord.js
@@ -1,1 +1,10 @@
 console.log("Tushar Borade");
+var fulName="Tushar Borade";
+console.log(fulName);
+console.log(fulName);
+var city;
+console.log(city);
+city="pune";
+console.log(city);
+city="surat";
+console.log(city);
